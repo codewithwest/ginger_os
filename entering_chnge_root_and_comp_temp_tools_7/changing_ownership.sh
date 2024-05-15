4317cb7b9666ee1edbed53cc6c9b3ec2307e013d
codewithwest <jonaslekgau@gmail.com>
2024-05-15 12:23:41 +0200

    change ownership update

---
+++ entering_chnge_root_and_comp_temp_tools_7/changing_ownership.sh
@@ -0,0 +1,86 @@
+echo "################################################################################";
+echo "#--------------------------Setting LFS variable--------------------------------#";
+echo "################################################################################";
+echo;echo;echo;
+LFS=/mnt/lfs;
+sleep 5;
+echo "################################################################################";
+echo "#-----------------------7.2. Changing Ownership--------------------------------#";
+echo "################################################################################";
+echo;echo;echo;
+chown -R root:root $LFS/{usr,lib,var,etc,bin,sbin,tools}
+case $(uname -m) in
+  x86_64) chown -R root:root $LFS/lib64 ;;
+esac
+sleep 5;
+echo "################################################################################";
+echo "#--------------------7.2. Changing Ownership Complete--------------------------#";
+echo "################################################################################";
+echo;echo;echo;
+sleep 5;
+echo "################################################################################";
+echo "#---------------7.3. Preparing Virtual Kernel File Systems---------------------#";
+echo "################################################################################";
+echo;echo;echo;
+mkdir -pv $LFS/{dev,proc,sys,run};
+sleep 5;
+echo "################################################################################";
+echo "#-------------------7.3.1. Mounting and Populating /dev------------------------#";
+echo "################################################################################";
+echo;echo;echo;
+mount -v --bind /dev $LFS/dev;
+sleep 5;
+echo "################################################################################";
+echo "#-------------------7.3.1. Mounting and Populating /dev complete------------------------#";
+echo "################################################################################";
+echo;echo;echo;
+sleep 5;
+echo "################################################################################";
+echo "#------------------7.3.2. Mounting Virtual Kernel File Systems-----------------#";
+echo "################################################################################";
+echo;echo;echo;
+mount -vt devpts devpts -o gid=5,mode=0620 $LFS/dev/pts;
+mount -vt proc proc $LFS/proc;
+mount -vt sysfs sysfs $LFS/sys;
+mount -vt tmpfs tmpfs $LFS/run;
+# In other host systems /dev/shm is a mount point for a tmpfs. 
+# In that case the mount of /dev above will only create 
+# /dev/shm as a directory in the chroot environment. 
+# In this situation we must explicitly mount a tmpfs:
+if [ -h $LFS/dev/shm ]; then
+  install -v -d -m 1777 $LFS$(realpath /dev/shm)
+else
+  mount -vt tmpfs -o nosuid,nodev tmpfs $LFS/dev/shm
+fi
+sleep 5;
+echo "################################################################################";
+echo "#---------------7.3.2. Mounting Virtual Kernel File Systems complete-----------#";
+echo "################################################################################";
+echo;echo;echo;
+
+echo "################################################################################";
+echo "#------------7.3. Preparing Virtual Kernel File Systems Complete---------------#";
+echo "################################################################################";
+echo;echo;echo;
+sleep 5;
+echo "################################################################################";
+echo "#-----------------7.4. Entering the Chroot Environment-------------------------#";
+echo "################################################################################";
+echo;echo;echo;
+chroot "$LFS" /usr/bin/env -i   \
+    HOME=/root                  \
+    TERM="$TERM"                \
+    PS1='(lfs chroot) \u:\w\$ ' \
+    PATH=/usr/bin:/usr/sbin     \
+    MAKEFLAGS="-j$(nproc)"      \
+    TESTSUITEFLAGS="-j$(nproc)" \
+    /bin/bash --login;
+sleep 5;
+echo "################################################################################";
+echo "#-----------------7.4. Entering the Chroot Environment complete----------------#";
+echo "################################################################################";
+echo;echo;echo;
+
+
+echo "You should be in lfs user root..."
+
