94a2b32ef29029c2d168179f30627c2fe4823aa6
= <jonas.lekgau@vaimo.com>
2026-01-12 14:50:40 +0200

    fix: Update binutils-pass2 script by replacing a `bfd/elf.c` patch with an `ltmain.sh` patch and refining libtool file cleanup.

--- scripts/phase2-tools/17-binutils-pass2.sh
+++ scripts/phase2-tools/17-binutils-pass2.sh
@@ -11,25 +11,25 @@ log "PROCESS" "Compiling Binutils Pass 2..."
 mkdir -v build
 cd build
 
-sed '609i\  if (elf_tdata (abfd)->elf_header->e_type == ET_DYN) return true;' \
-    -i ../bfd/elf.c
+sed '6031s/$add_dir//' -i ltmain.sh
 
-../configure --prefix=/usr                      \
-             --build=$(../config.guess)         \
-             --host=$LFS_TGT                    \
-             --disable-nls                      \
-             --enable-shared                    \
-             --enable-gprofng=no                \
-             --disable-werror                   \
-             --enable-64-bit-bfd                \
-             --enable-new-dtags                 \
-             --enable-default-hash-style=gnu
+../configure                   \
+    --prefix=/usr              \
+    --build=$(../config.guess) \
+    --host=$LFS_TGT            \
+    --disable-nls              \
+    --enable-shared            \
+    --enable-gprofng=no        \
+    --disable-werror           \
+    --enable-64-bit-bfd        \
+    --enable-new-dtags         \
+    --enable-default-hash-style=gnu
 
 make $MAKEFLAGS
 make DESTDIR=$LFS install
 
 # Remove libtool files
-rm -v $LFS/usr/lib/lib{bfd,ctf,ctf-nobfd,gprofng,opcodes,sframe}.la
+rm -v $LFS/usr/lib/lib{bfd,ctf,ctf-nobfd,opcodes,sframe}.{a,la}
 
 cd ../..
 rm -rf "binutils-"*
