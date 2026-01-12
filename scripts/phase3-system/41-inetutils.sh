f4487c7ba071768dbf642d17db7ee8497cce4222
= <jonas.lekgau@vaimo.com>
2026-01-12 20:23:06 +0200

    Add sed command

--- scripts/phase3-system/41-inetutils.sh
+++ scripts/phase3-system/41-inetutils.sh
@@ -4,6 +4,9 @@ source "$(dirname "$(readlink -f "$0")")/../common.sh"
 PKG_NAME="inetutils"
 check_built "$PKG_NAME" && exit 0
 extract "inetutils"
+# Handle GCC 15 strictness for legacy terminal functions
+sed -i 's/def HAVE_TERMCAP_TGETENT/ 1/' telnet/telnet.c
+
 ./configure --prefix=/usr        \
             --bindir=/usr/bin    \
             --localstatedir=/var \
@@ -14,8 +17,15 @@ extract "inetutils"
             --disable-rlogin     \
             --disable-rsh        \
             --disable-servers
+
 make $MAKEFLAGS
 make install
-mv -v /usr/bin/{hostname,ifconfig,ping,ping6,traceroute} /usr/bin
+
+mv -v /usr/{,s}bin/ifconfig
+
+
+# In Merged-usr, ifconfig/ping etc are already in /usr/bin. 
+# We just need to ensure permissions are correct if needed.
+
 cd .. && rm -rf "inetutils-"*
 mark_built "$PKG_NAME"
