fe1a4478f4474aa18b80198fc4d3d3012559418e
= <jonas.lekgau@vaimo.com>
2026-01-12 19:21:54 +0200

    feat: build and install GMP HTML documentation and apply a sed fix to the configure script.

--- scripts/phase3-system/21-gmp.sh
+++ scripts/phase3-system/21-gmp.sh
@@ -3,12 +3,21 @@
 source "$(dirname "$(readlink -f "$0")")/../common.sh"
 PKG_NAME="gmp"
 check_built "$PKG_NAME" && exit 0
+
 extract "gmp"
+
+sed -i '/long long t1;/,+1s/()/(...)/' configure
+
 ./configure --prefix=/usr    \
             --enable-cxx     \
             --disable-static \
             --docdir=/usr/share/doc/gmp-6.3.0
 make $MAKEFLAGS
+
+make html
+
 make install
+make install-html
+
 cd .. && rm -rf "gmp-"*
 mark_built "$PKG_NAME"
