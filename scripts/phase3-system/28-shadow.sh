1a7b3d76502f8832f05cd575c95f2d521ae362b6
= <jonas.lekgau@vaimo.com>
2026-01-12 19:26:26 +0200

    feat: Update shadow configuration to use b/yescrypt, disable libbsd, set group name length, and refine login.defs PATH.

--- scripts/phase3-system/28-shadow.sh
+++ scripts/phase3-system/28-shadow.sh
@@ -4,19 +4,25 @@ source "$(dirname "$(readlink -f "$0")")/../common.sh"
 PKG_NAME="shadow"
 check_built "$PKG_NAME" && exit 0
 extract "shadow"
+
 sed -i 's/groups$(EXEEXT) //' src/Makefile.in
 find man -name Makefile.in -exec sed -i 's/groups\.1 / /'   {} \;
 find man -name Makefile.in -exec sed -i 's/getspnam\.3 / /' {} \;
 find man -name Makefile.in -exec sed -i 's/passwd\.5 / /'   {} \;
 
-sed -e 's@#ENCRYPT_METHOD DES@ENCRYPT_METHOD YESCRYPT@' \
-    -e 's@/var/spool/mail@/var/mail@'                   \
-    -e '/SUBID_[GU]ID_MIN/s/100000/65536/'              \
+sed -e 's:#ENCRYPT_METHOD DES:ENCRYPT_METHOD YESCRYPT:' \
+    -e 's:/var/spool/mail:/var/mail:'                   \
+    -e '/PATH=/{s@/sbin:@@;s@/bin:@@}'                  \
     -i etc/login.defs
+
+touch /usr/bin/passwd
 
 ./configure --sysconfdir=/etc   \
             --disable-static    \
+            --with-{b,yes}crypt \
+            --without-libbsd    \
             --with-group-name-max-length=32
+
 make $MAKEFLAGS
 make exec_prefix=/usr install
 make -C man install-man
