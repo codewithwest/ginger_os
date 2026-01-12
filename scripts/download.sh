ed2c47d12b87c1fa2b28609c55192697b7528d09
= <jonas.lekgau@vaimo.com>
2026-01-12 10:03:10 +0200

    feat: add version-check script and execute it from download.sh to validate the build environment.

--- scripts/download.sh
+++ scripts/download.sh
@@ -4,17 +4,14 @@
 source "$(dirname "$(readlink -f "$0")")/common.sh"
 
 WGET_LIST="https://www.linuxfromscratch.org/lfs/view/stable/wget-list"
-VERSION_CHECK="https://www.linuxfromscratch.org/lfs/view/stable/version-check.sh"
 
 cd "$GINGER_SOURCES"
 
 log "INFO" "Checking host requirements..."
-# Note: In a real scenario, we'd run version-check.sh here.
-# For now, we assume the environment is sane as per user prompt.
+bash "$GINGER_SCRIPTS/version-check.sh"
 
 log "INFO" "Downloading LFS 12.4 package list..."
 wget -nc "$WGET_LIST"
-wget -nc "$VERSION_CHECK"
 
 log "INFO" "Downloading all packages (this may take a while)..."
 # -nc means no-clobber, don't download if exists
