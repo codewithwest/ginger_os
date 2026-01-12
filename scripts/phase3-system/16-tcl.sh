b95c6bf28884a13d801f24d9a813beea9baf9f0f
= <jonas.lekgau@vaimo.com>
2026-01-12 16:18:08 +0200

    refactor: update Tcl build script to refine configuration, path handling, and install HTML documentation.

--- scripts/phase3-system/16-tcl.sh
+++ scripts/phase3-system/16-tcl.sh
@@ -7,33 +7,49 @@ check_built "$PKG_NAME" && exit 0
 # Tcl is a bit unique with its archive name and sub-directory
 extract "tcl"
 # The extract function should handle the -src suffix and cd into unix/
+SRCDIR=$(pwd)
+
 cd unix
 
 ./configure --prefix=/usr           \
-            --with-system-libtoml   \
-            --enable-threads        \
-            --mandir=/usr/share/man
+            --mandir=/usr/share/man \
+            --disable-rpath
 
-make $MAKEFLAGS
+make
 
-sed -e "s#$GINGER_ROOT/build/tcl$TCL_VERSION/unix#/usr/lib#" \
-    -e "s#$GINGER_ROOT/build/tcl$TCL_VERSION#/usr/include#" \
+sed -e "s|$SRCDIR/unix|/usr/lib|" \
+    -e "s|$SRCDIR|/usr/include|"  \
     -i tclConfig.sh
 
-sed -e "s#$GINGER_ROOT/build/tcl$TCL_VERSION/unix/pkgs/itcl[0-9.]*#/usr/lib/itcl[0-9.]*#" \
-    -e "s#$GINGER_ROOT/build/tcl$TCL_VERSION/pkgs/itcl[0-9.]*#/usr/include#" \
-    -e "s#$GINGER_ROOT/build/tcl$TCL_VERSION/unix#/usr/include#" \
-    -i pkgs/itcl[0-9.]*/itclConfig.sh
+sed -e "s|$SRCDIR/unix/pkgs/tdbc1.1.10|/usr/lib/tdbc1.1.10|" \
+    -e "s|$SRCDIR/pkgs/tdbc1.1.10/generic|/usr/include|"     \
+    -e "s|$SRCDIR/pkgs/tdbc1.1.10/library|/usr/lib/tcl8.6|"  \
+    -e "s|$SRCDIR/pkgs/tdbc1.1.10|/usr/include|"             \
+    -i pkgs/tdbc1.1.10/tdbcConfig.sh
 
-sed -e "s#$GINGER_ROOT/build/tcl$TCL_VERSION/unix/pkgs/tdbc[0-9.]*#/usr/lib/tdbc[0-9.]*#" \
-    -e "s#$GINGER_ROOT/build/tcl$TCL_VERSION/pkgs/tdbc[0-9.]*#/usr/include#" \
-    -e "s#$GINGER_ROOT/build/tcl$TCL_VERSION/unix#/usr/include#" \
-    -i pkgs/tdbc[0-9.]*/tdbcConfig.sh
+sed -e "s|$SRCDIR/unix/pkgs/itcl4.3.2|/usr/lib/itcl4.3.2|" \
+    -e "s|$SRCDIR/pkgs/itcl4.3.2/generic|/usr/include|"    \
+    -e "s|$SRCDIR/pkgs/itcl4.3.2|/usr/include|"            \
+    -i pkgs/itcl4.3.2/itclConfig.sh
+
+unset SRCDIR
 
 make install
+chmod 644 /usr/lib/libtclstub8.6.aln -sfv tclsh8.6 /usr/bin/tclsh
+
 chmod -v u+w /usr/lib/libtcl8.6.so
+
 make install-private-headers
+
 ln -sfv tclsh8.6 /usr/bin/tclsh
+
+mv /usr/share/man/man3/{Thread,Tcl_Thread}.3
+
+cd ..
+tar -xf ../tcl8.6.16-html.tar.gz --strip-components=1
+mkdir -v -p /usr/share/doc/tcl-8.6.16
+cp -v -r  ./html/* /usr/share/doc/tcl-8.6.16
+
 
 cd ../.. && rm -rf "tcl"*
 mark_built "$PKG_NAME"
