be228a01834ed51fe80eb1d49da397ac36be84ba
= <jonas.lekgau@vaimo.com>
2026-01-12 15:40:39 +0200

    updating install command in man-pages script

--- scripts/phase3-system/03-man-pages.sh
+++ scripts/phase3-system/03-man-pages.sh
@@ -5,6 +5,7 @@ PKG_NAME="man-pages"
 check_built "$PKG_NAME" && exit 0
 extract "man-pages"
 log "PROCESS" "Installing Man-pages..."
-make prefix=/usr install
+make -R GIT=false prefix=/usr install
+
 cd .. && rm -rf "man-pages-"*
 mark_built "$PKG_NAME"
