f6572cf40c10898f9266bece0239c50049a0c3a7
= <jonas.lekgau@vaimo.com>
2026-01-12 10:43:45 +0200

    feat: Implement fallback download mechanism using an LFS mirror and add MD5 checksum verification for downloaded packages.

--- scripts/download.sh
+++ scripts/download.sh
@@ -14,7 +14,17 @@ log "INFO" "Downloading LFS 12.4 package list..."
 wget -nc "$WGET_LIST"
 
 log "INFO" "Downloading all packages (this may take a while)..."
-# -nc means no-clobber, don't download if exists
-wget -nc -i wget-list --continue --directory-prefix="$GINGER_SOURCES"
+# Try official wget-list first
+if ! wget -nc --input-file=wget-list --continue --tries=3 --timeout=15; then
+    log "WARN" "Primary download failed. Attempting to use LFS mirror..."
+    
+    # Create a mirror-list by replacing gnu.org and other problematic domains with the LFS mirror
+    sed 's|https://.*\.gnu\.org/gnu/|https://anduin.linuxfromscratch.org/LFS/|g' wget-list > wget-list-mirror
+    sed -i 's|https://.*\.kernel\.org/pub/linux/|https://anduin.linuxfromscratch.org/LFS/|g' wget-list-mirror
+    
+    wget -nc --input-file=wget-list-mirror --continue --tries=5 --timeout=30
+fi
 
-log "INFO" "All sources downloaded."
+log "INFO" "Verifying packages..."
+wget -nc https://www.linuxfromscratch.org/lfs/view/stable/md5sums
+md5sum -c md5sums --status || log "WARN" "Some checksums failed. Check logs/md5_errors.txt"
