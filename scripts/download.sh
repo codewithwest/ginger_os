60ba681c79897143ba275aa3ba8d4c07ea7f8a8e
= <jonas.lekgau@vaimo.com>
2026-01-12 10:46:32 +0200

    refactor: Switch package download strategy to exclusively use the LFS Anduin Mirror by iterating through individual files.

--- scripts/download.sh
+++ scripts/download.sh
@@ -13,18 +13,21 @@ bash "$GINGER_SCRIPTS/version-check.sh"
 log "INFO" "Downloading LFS 12.4 package list..."
 wget -nc "$WGET_LIST"
 
-log "INFO" "Downloading all packages (this may take a while)..."
-# Try official wget-list first
-if ! wget -nc --input-file=wget-list --continue --tries=3 --timeout=15; then
-    log "WARN" "Primary download failed. Attempting to use LFS mirror..."
-    
-    # Create a mirror-list by replacing gnu.org and other problematic domains with the LFS mirror
-    sed 's|https://.*\.gnu\.org/gnu/|https://anduin.linuxfromscratch.org/LFS/|g' wget-list > wget-list-mirror
-    sed -i 's|https://.*\.kernel\.org/pub/linux/|https://anduin.linuxfromscratch.org/LFS/|g' wget-list-mirror
-    
-    wget -nc --input-file=wget-list-mirror --continue --tries=5 --timeout=30
-fi
+log "INFO" "Primary servers are flaky. Using LFS Anduin Mirror for reliability..."
+# Extract just the filenames from the wget-list
+grep -oP '[^/]+$' wget-list > filenames.txt
+
+# Download everything from Anduin mirror
+MIRROR_URL="https://anduin.linuxfromscratch.org/LFS"
+
+while read -r FILE; do
+    if [ ! -f "$FILE" ]; then
+        log "PROCESS" "Mirroring $FILE..."
+        wget -nc -T 20 -t 5 "$MIRROR_URL/$FILE" || log "WARN" "Failed to download $FILE"
+    fi
+done < filenames.txt
 
 log "INFO" "Verifying packages..."
 wget -nc https://www.linuxfromscratch.org/lfs/view/stable/md5sums
-md5sum -c md5sums --status || log "WARN" "Some checksums failed. Check logs/md5_errors.txt"
+md5sum -c md5sums --status || log "WARN" "Some checksums failed. Total files: $(ls -1 | wc -l)"
+rm filenames.txt
