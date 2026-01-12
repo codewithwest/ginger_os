c29c59328da86a8d8b835a5285821802d6f2e060
= <jonas.lekgau@vaimo.com>
2026-01-12 21:10:00 +0200

    Update `flit-core` build script to use `-w dist --no-build-isolation --no-deps $PWD` for `pip3 wheel` and remove `--no-user` from `pip3 install`.

--- scripts/phase3-system/53-flit-core.sh
+++ scripts/phase3-system/53-flit-core.sh
@@ -4,8 +4,8 @@ source "$(dirname "$(readlink -f "$0")")/../common.sh"
 PKG_NAME="flit-core"
 check_built "$PKG_NAME" && exit 0
 extract "flit_core"
-pip3 wheel --no-index --no-cache-dir --no-user --find-links dist .
-pip3 install --no-index --no-user --find-links dist flit_core
+pip3 wheel -w dist --no-cache-dir --no-build-isolation --no-deps $PWD
+pip3 install --no-index --find-links dist flit_core
 cd .. && rm -rf "flit_core-"*
 mark_built "$PKG_NAME"
 # Note: Use of pip3 in chroot requires python to be installed first.
