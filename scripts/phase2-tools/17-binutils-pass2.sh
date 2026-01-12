ceea75eedbfa3fbf057e11663ed231461db3af41
= <jonas.lekgau@vaimo.com>
2026-01-12 14:52:14 +0200

    refactor: reorder Binutils Pass 2 build steps, clarify logging, and robustify libtool file removal.

--- scripts/phase2-tools/17-binutils-pass2.sh
+++ scripts/phase2-tools/17-binutils-pass2.sh
@@ -7,11 +7,13 @@ check_built "$PKG_NAME" && exit 0
 
 extract "binutils"
 
-log "PROCESS" "Compiling Binutils Pass 2..."
+log "PROCESS" "Configuring Binutils Pass 2..."
+
+# LFS 12.4 specific fix for libtool
+sed '6031s/$add_dir//' -i ltmain.sh
+
 mkdir -v build
 cd build
-
-sed '6031s/$add_dir//' -i ltmain.sh
 
 ../configure                   \
     --prefix=/usr              \
@@ -25,11 +27,15 @@ sed '6031s/$add_dir//' -i ltmain.sh
     --enable-new-dtags         \
     --enable-default-hash-style=gnu
 
+log "PROCESS" "Compiling Binutils Pass 2..."
 make $MAKEFLAGS
+
+log "PROCESS" "Installing Binutils Pass 2..."
 make DESTDIR=$LFS install
 
-# Remove libtool files
-rm -v $LFS/usr/lib/lib{bfd,ctf,ctf-nobfd,opcodes,sframe}.{a,la}
+# Remove libtool files and static libraries
+# Use -f to prevent failure if some are missing
+rm -fv $LFS/usr/lib/lib{bfd,ctf,ctf-nobfd,opcodes,sframe}.{a,la}
 
 cd ../..
 rm -rf "binutils-"*
