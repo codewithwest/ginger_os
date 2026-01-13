5b29f3a02a72b31b4ca74330df518e1fe06d9885
= <jonas.lekgau@vaimo.com>
2026-01-13 19:44:46 +0200

    updating patch name

--- scripts/phase3-system/84-sysvinit.sh
+++ scripts/phase3-system/84-sysvinit.sh
@@ -6,7 +6,7 @@ check_built "$PKG_NAME" && exit 0
 extract "sysvinit"
 
 # Apply patch if present
-patch -Np1 -i /sources/sysvin it-3.14-consolidated-1.patch
+patch -Np1 -i /sources/sysvinit-3.14-consolidated-1.patch
 
 make $MAKEFLAGS
 make install
