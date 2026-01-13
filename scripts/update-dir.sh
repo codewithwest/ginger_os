7308486a2228ee444b8c359e680505e48c685151
= <jonas.lekgau@vaimo.com>
2026-01-13 10:12:08 +0200

    feat: establish initial LFS directory structure, create essential symlinks, and set ownership.

--- scripts/update-dir.sh
+++ scripts/update-dir.sh
@@ -4,6 +4,19 @@
 LFS=/mnt/lfs
 source "$(dirname "$(readlink -f "$0")")/../common.sh"
 
+mkdir -pv $LFS/{etc,var} $LFS/usr/{bin,lib,sbin}
+
+for i in bin lib sbin; do
+  ln -sv usr/$i $LFS/$i
+done
+
+case $(uname -m) in
+  x86_64) mkdir -pv $LFS/lib64 ;;
+esac
+
+mkdir -pv $LFS/tools
+
+
 # Update the directory
 chown -v lfs $LFS/{usr{,/*},var,etc,tools}
 case $(uname -m) in
