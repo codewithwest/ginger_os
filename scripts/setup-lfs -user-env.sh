b8a509f2ac9b1da6966792ad66505296eddf114e
= <jonas.lekgau@vaimo.com>
2026-01-13 09:59:40 +0200

    feat: add script to configure user's bash environment for LFS.

---
+++ scripts/setup-lfs -user-env.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# LFS 12.4
+
+
+cat > ~/.bash_profile << "EOF"
+exec env -i HOME=$HOME TERM=$TERM PS1='\u:\w\$ ' /bin/bash
+EOF
+
+cat > ~/.bashrc << "EOF"
+set +h
+umask 022
+LFS=/mnt/lfs
+LC_ALL=POSIX
+LFS_TGT=$(uname -m)-lfs-linux-gnu
+PATH=/usr/bin
+if [ ! -L /bin ]; then PATH=/bin:$PATH; fi
+PATH=$LFS/tools/bin:$PATH
+CONFIG_SITE=$LFS/usr/share/config.site
+export LFS LC_ALL LFS_TGT PATH CONFIG_SITE
+EOF
+
+cat >> ~/.bashrc << "EOF"
+export MAKEFLAGS=-j$(nproc)
+EOF
+
+source ~/.bash_profile
+
