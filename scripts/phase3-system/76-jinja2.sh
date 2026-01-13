28f77dec8014661fa1d010d54206ee429e5d1107
= <jonas.lekgau@vaimo.com>
2026-01-13 19:32:37 +0200

    remove non  used line

--- scripts/phase3-system/76-jinja2.sh
+++ scripts/phase3-system/76-jinja2.sh
@@ -9,6 +9,5 @@ pip3 wheel -w dist --no-cache-dir --no-build-isolation --no-deps $PWD
 
 pip3 install --no-index --find-links dist Jinja2
 
-cp -rv dest/* /
 cd .. && rm -rf "jinja2-"*
 mark_built "$PKG_NAME"
