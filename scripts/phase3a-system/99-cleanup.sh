3fed9ee5cd58ac6c348c387c916ec493a1627bf1
= <jonas.lekgau@vaimo.com>
2026-01-14 13:09:39 +0200

    Updating clean up script

--- scripts/phase3a-system/99-cleanup.sh
+++ scripts/phase3a-system/99-cleanup.sh
@@ -2,11 +2,11 @@
 # GingerOS - Final System cleanup
 source "/scripts/common.sh"
 log "INFO" "Cleaning up system..."
-rm -rf /tmp/*
-# Remove static libraries if any remain
-rm -f /usr/lib/*.a
-# Strip debug symbols from binaries to save space (Standard LFS)
-find /usr/lib /usr/bin /usr/sbin -type f \
-    $(printf '! -name %q ' ld-linux-x86-64.so.2 libc.so.6 libpthread.so.0) \
-    -exec strip --strip-debug {} ';'
-log "INFO" "System ready."
+
+rm -rf /tmp/{*,.*}
+
+find /usr/lib /usr/libexec -name \*.la -delete
+
+find /usr -depth -name $(uname -m)-lfs-linux-gnu\* | xargs rm -rf
+
+userdel -r tester
