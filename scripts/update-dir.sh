310c03ccbd53dc718d7a15e64efab9656e02c3f1
= <jonas.lekgau@vaimo.com>
2026-01-16 18:46:29 +0200

    build: Use `mkdir -pv` for `/mnt/lfs/var/lib/ginger/` directory creation.

--- scripts/update-dir.sh
+++ scripts/update-dir.sh
@@ -16,7 +16,7 @@ esac
 
 mkdir -pv $LFS/tools
 
-mkdir /mnt/lfs/var/lib/ginger/
+mkdir -pv $LFS/var/lib/ginger/
 chown -v lfs $LFS/{usr{,/*},var,etc,tools}
 case $(uname -m) in
   x86_64) chown -v lfs $LFS/lib64 ;;
