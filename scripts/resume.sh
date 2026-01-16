88b5f1a951b1e9503029eff0003794cc90b4ab25
= <jonas.lekgau@vaimo.com>
2026-01-16 09:33:29 +0200

    feat: Add `resume.sh` script to mount the ginger_os image partition to `/mnt/lfs`.

---
+++ scripts/resume.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+export LFS=/mnt/lfs
+IMAGE=/opt/ginger_os/ginger_os.img
+
+LOOP=$(losetup -fP --show "$IMAGE")
+mount "${LOOP}p1" "$LFS"
+
+echo "LFS mounted at $LFS using $LOOP"
+df -h "$LFS"
