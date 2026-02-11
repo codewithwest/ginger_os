05c2057ec105bcbc61dbe67f89099a3d7bbe1aa3
= <jonas.lekgau@vaimo.com>
2026-02-11 22:37:30 +0200

    feat: add /root directory to the initrd build environment

--- scripts/iso/make-iso.sh
+++ scripts/iso/make-iso.sh
@@ -48,7 +48,7 @@ cp -v "$KERNEL_IMG" "$ISO_DIR/boot/vmlinuz"
 ui_step 2
 ui_log "Assembling Live Environment..."
 sudo rm -rf "$INITRD_WORK"
-mkdir -p "$INITRD_WORK"/{bin,dev,etc,lib,lib64,mnt,proc,run,sbin,sys,tmp,var}
+mkdir -p "$INITRD_WORK"/{bin,dev,etc,lib,lib64,mnt,proc,run,sbin,sys,tmp,var,root}
 
 # Tools List (Consolidated)
 TOOLS=(bash sh ls cat cp mv mkdir rm ln chmod chown chgrp grep sed awk tee head tail sort uniq wc cut tr xgettext xargs basename dirname find mount umount findmnt blkid parted lsblk fdisk udevadm wipefs mke2fs mkfs.ext4 id whoami sleep sync uname hostname dmesg ps top kill mktemp readlink realpath tar gzip bzip2 xz md5sum grub-install grub-probe grub-mkconfig sudo chroot mountpoint find vi nano)
