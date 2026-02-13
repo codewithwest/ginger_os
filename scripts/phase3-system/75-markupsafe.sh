1aa0fc2d4d68eafcff8c82b97f8ff682ee6c62e7
codewithwest <jonaslekgau@gmail.com>
2026-02-14 00:20:24 +0200

    fix: Corrected package name casing for markupsafe in build script.

--- scripts/phase3-system/75-markupsafe.sh
+++ scripts/phase3-system/75-markupsafe.sh
@@ -3,11 +3,11 @@
 source "/scripts/lib/common.sh"
 PKG_NAME="markupsafe"
 check_built "$PKG_NAME" && exit 0
-extract "MarkupSafe"
+extract "markupsafe"
 
 pip3 wheel -w dist --no-cache-dir --no-build-isolation --no-deps $PWD
 
-pip3 install --no-index --find-links dist Markupsafe
+pip3 install --no-index --find-links dist markupsafe
 
 cleanup
 mark_built "$PKG_NAME"
